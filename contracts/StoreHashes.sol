3f1b1b4150dc6a1186cdfe9dfeab3f59ee1ff602
Mehran Hydary <mehran.hydary@gmail.com>
2019-02-21 16:52:47 -0500

    added etherscan links

--- contracts/StoreHashes.sol
+++ contracts/StoreHashes.sol
@@ -1,29 +1,30 @@
 pragma solidity ^0.5.0;
 
 contract StoreHashes {
-	event NewCert(uint certId, string droneCorridor, string ipfsHash);
+    event NewCert(uint certId, string droneCorridor, string ipfsHash);
 	
-	struct CertHash {
-		string droneCorridor;
-		string ipfsHash;
-	}
+    struct CertHash {
+        string droneCorridor;
+        string ipfsHash;
+    }
 
- 	CertHash[] public certHashes;
+    CertHash[] public certHashes;
 
- 	mapping(uint => address) public certToOwner;
- 	mapping (address => uint) public ownerCertCount;
+    mapping(uint => address) public certToOwner;
+    mapping (address => uint) public ownerCertCount;
 
- 	function _createCert(string memory _droneCorridor, string memory _ipfsHash) public {
- 		uint id = certHashes.push(CertHash(_droneCorridor, _ipfsHash)) - 1;
+    function _createCert(string memory _droneCorridor, string memory _ipfsHash) public {
+        uint id = certHashes.push(CertHash(_droneCorridor, _ipfsHash)) - 1;
         certToOwner[id] = msg.sender;
         ownerCertCount[msg.sender]++;
         emit NewCert(id, _droneCorridor, _ipfsHash);
- 	}
- 	function _getCert(uint index) public view returns(string memory, string memory) {
+    }
+    
+    function _getCert(uint index) public view returns(string memory, string memory) {
         return (certHashes[index].droneCorridor, certHashes[index].ipfsHash);
     }
+    
     function _getOwnerCount () public view returns (uint) {
-    	return ownerCertCount[msg.sender];
+        return ownerCertCount[msg.sender];
     }
-
 }
